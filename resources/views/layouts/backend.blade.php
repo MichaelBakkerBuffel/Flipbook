44f95c5c21158299dcaa279b3099f016cd8d5a28
Laurens-Post <contact@lphosting.nl>
2017-12-07 11:37:02 +0100

    backend skin edit

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>MyBlog | Dashboard</title>
+  <title>Mantelzorg | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
@@ -36,7 +36,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>M</b>B</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>MY</b>BLOG</span>
+      <span class="logo-lg"><b>Mantelzorg</b> Backend</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
@@ -52,8 +52,8 @@
         <ul class="nav navbar-nav">
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <img src="backend/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <span class="hidden-xs">Laurens Post</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
@@ -61,7 +61,7 @@
                 <img src="img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
-                  Alexander Pierce - Web Developer
+                  Laurens Post - Web Developer
                   <small>Member since Nov. 2012</small>
                 </p>
               </li>
@@ -87,10 +87,10 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="backend/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Alexander Pierce</p>
+          <p>Laurens Post</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
@@ -105,13 +105,13 @@
         <li class="treeview">
           <a href="#">
             <i class="fa fa-pencil"></i>
-            <span>Blog</span>
+            <span>Artikelen</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> All Posts</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Alle artikelen</a></li>
             <li><a href="#"><i class="fa fa-circle-o"></i> Add New</a></li>
           </ul>
         </li>
@@ -140,11 +140,10 @@
             <div class="box">
               <!-- /.box-header -->
               <div class="box-body ">
-                    <h3>Welcome to MyBlog!</h3>
-                    <p class="lead text-muted">Hallo User, Welcome to MyBlog</p>
+                    <p class="lead text-muted">Hallo admin!</p>
 
-                    <h4>Get started</h4>
-                    <p><a href="#" class="btn btn-primary">Write your first blog post</a> </p>
+                    <h4>Start hier</h4>
+                    <p><a href="#" class="btn btn-primary">Schrijf een artikel</a> </p>
               </div>
               <!-- /.box-body -->
             </div>
