d107696800fd836b53712946dc28715aed36f436
Laurens-Post <contact@lphosting.nl>
2017-12-07 12:09:10 +0100

    logout working now and divided backend into pieces for better vision

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,48 @@
-@extends('layouts.backend')
+@extends('layouts.backend.main')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                    <!-- Content Wrapper. Contains page content -->
+                    <div class="content-wrapper">
+                        <!-- Content Header (Page header) -->
+                        <section class="content-header">
+                            <h1>
+                                Dasbhboard
+                            </h1>
+                            <ol class="breadcrumb">
+                                <li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
+                            </ol>
+                        </section>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                        <!-- Main content -->
+                        <section class="content">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <div class="box">
+                                            <!-- /.box-header -->
+                                            <div class="box-body ">
+                                                        <p class="lead text-muted">Hallo {{ Auth::user()->name }}</p>
+
+                                                        <h4>Start hier</h4>
+                                                        <p><a href="#" class="btn btn-primary">Schrijf een artikel</a> </p>
+                                            </div>
+                                            <!-- /.box-body -->
+                                        </div>
+                                        <!-- /.box -->
+                                    </div>
+                                </div>
+                            <!-- ./row -->
+                        </section>
+                        <!-- /.content -->
+                    </div>
+                    <!-- /.content-wrapper -->
+                    <footer class="main-footer">
+                        <div class="pull-right hidden-xs">
+                            <b>Version</b> 1.0
                         </div>
-                    @endif
+                        <strong>Copyright &copy; 2017 <a href="http://www.lpwebdesign.nl">Laurens Post</a>.</strong> All rights
+                        reserved.
+                    </footer>
 
-                    You are logged in!
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
+                <!-- ./wrapper -->
 @endsection
