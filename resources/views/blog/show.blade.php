c5003201112edcebf9478cc1afdfd07e2263718e
LAPTOP-FABIAN\Fabian <fabsterdanoontjedevries@gmail.com>
2017-12-12 15:31:51 +0100

    Blogposts

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -6,10 +6,14 @@
             <div class="row">
                 <div class="col-md-8">
                     @foreach($posts as $post)
-                        <h2>{{ $post->title }}</h2>
-                        <p>{{ $post->excerpt }}</p>
-                        <p>{{ $post->author->name }}</p>
-                        <p>{{ $post->created_at->diffForHumans() }}</p>
+                        <article>
+                            <div class="article-heading">
+                                <h2>{{ $post->title }}</h2>
+                            </div>
+                            <p>{{ $post->excerpt }}</p>
+                            <p>{{ $post->author->name }}</p>
+                            <p>{{ $post->created_at->diffForHumans() }}</p>
+                        </article>
                     @endforeach
 
                     <!-- pagination -->
