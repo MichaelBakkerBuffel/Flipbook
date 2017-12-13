4fc988a4db7ccbe8eb422f747eef25e4583523b8
MichaelBakkerBuffel <michael_snits@hotmail.com>
2017-12-13 10:52:45 +0100

    Acolade wasn't closed, fixed it

--- database/migrations/2017_12_12_135753_create_categories_table.php
+++ database/migrations/2017_12_12_135753_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Categories extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -16,6 +16,7 @@ class Categories extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title'); 
+        });
     }
 
     /**
