30ca6ae6fe3ec381d1e33ca2efd941f638e99718
LAPTOP-FABIAN\Fabian <fabsterdanoontjedevries@gmail.com>
2017-12-14 15:28:28 +0100

    Mantelcheck

--- resources/views/blog/mantelcheck.blade.php
+++ resources/views/blog/mantelcheck.blade.php
@@ -1,1 +1,25 @@
-d
+@extends ('layouts.main')
+
+@section('content')
+
+
+	<div class="container">
+    	<div class="row">
+    		<div class="col-md-10 mantelcheck-title">
+    			<h4>Mantelcheck</h4>
+    		</div>	
+    		<div class="col-md-2 flag">
+    			<h3>10% mantelzorger</h3>
+    		</div>	
+			<div class="col-md-5 mantelcheck-bodyblock mantelcheck-no">
+				<h2>Nee</h2>
+				<p>Zorgt u voor iemand die extra hulp nodig heeft?</p>
+			</div>
+			<div class="col-md-5 mantelcheck-bodyblock mantelcheck-yes">
+				<h2>Ja</h2>
+				<p>Zorgt u voor iemand die extra hulp nodig heeft?</p>
+			</div>
+		</div>
+	</div>
+
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -39,24 +39,24 @@
 
         @yield('content')
 
-            <footer class="second-color">
-                <div class="container">
-                        <div class="row">
-                                <div class="col-md-8">
-                                        <p class="copyright">&copy; 2017 - Laurens Post</p>
-                                </div>
-                                <div class="col-md-4">
-                                        <nav>
-                                                <ul class="social-icons">
-                                                        <li><a href="#" class="i fa fa-facebook"></a></li>
-                                                        <li><a href="#" class="i fa fa-twitter"></a></li>
-                                                        <li><a href="#" class="i fa fa-google-plus"></a></li>
-                                                        <li><a href="#" class="i fa fa-github"></a></li>
-                                                </ul>
-                                        </nav>
-                                </div>
-                        </div>
-                </div>
+        <footer class="second-color">
+            <div class="container">
+                    <div class="row">
+                            <div class="col-md-8">
+                                    <p class="copyright">&copy; 2017 - Laurens Post</p>
+                            </div>
+                            <div class="col-md-4">
+                                    <nav>
+                                            <ul class="social-icons">
+                                                    <li><a href="#" class="i fa fa-facebook"></a></li>
+                                                    <li><a href="#" class="i fa fa-twitter"></a></li>
+                                                    <li><a href="#" class="i fa fa-google-plus"></a></li>
+                                                    <li><a href="#" class="i fa fa-github"></a></li>
+                                            </ul>
+                                    </nav>
+                            </div>
+                    </div>
+            </div>
         </footer>
 
         <script src="{{ url('/') }}/js/bootstrap.min.js"></script>
