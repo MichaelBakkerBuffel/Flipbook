9b9e2ed908f48264851e244c55c37a6346b3291f
Laurens-Post <contact@lphosting.nl>
2017-12-14 13:50:20 +0100

    Fix for filter categories
    Where -> category_id->id in category function fixed

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -22,16 +22,13 @@ class ContentController extends Controller
 
         public function category($id)
     {
-        \DB::enableQueryLog();
-
-        $categories = Category::with('posts')->where('id', $id)->orderBy('title', 'asc')->get();
+        $categories = Category::with('posts')->orderBy('title', 'asc')->get();
 
         // get all posts + desc ordering and show 3 posts
         // filter: (checks if title is like the entered term)
-        $posts = Post::with('author')->filter(request('term'))->orderBy('created_at', 'desc')->paginate(3);
+        $posts = Post::with('author')->where('category_id', $id)->filter(request('term'))->orderBy('created_at', 'desc')->paginate(3);
 
-        return view("blog.show", compact('posts', 'categories'))->render();
-        dd(\DB::getQueryLog());
+        return view("blog.show", compact('posts', 'categories'));
 
     }
 
