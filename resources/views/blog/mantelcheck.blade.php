eb1adff0883b9d9c97705e3a49f182b05720c9b0
LAPTOP-FABIAN\Fabian <fabsterdanoontjedevries@gmail.com>
2017-12-19 11:34:29 +0100

    Mantelzorgtest update

--- resources/views/blog/mantelcheck.blade.php
+++ resources/views/blog/mantelcheck.blade.php
@@ -6,15 +6,13 @@
 	<div class="container">
     	<div class="row">
     		<div class="col-md-12 mantelcheck-title">
-    			<h4>Mantelzorgtest</h4>
+    			<h4>1.Zorgt u voor iemand die extra hulp nodig heeft?</h4>
     		</div>
 			<div class="col-md-6 mantelcheck-bodyblock mantelcheck-no">
-				<h2>Nee</h2>
-				<p>Zorgt u voor iemand die extra hulp nodig heeft?</p>
+				<h1>Nee</h1>
 			</div>
 			<div class="col-md-6 mantelcheck-bodyblock mantelcheck-yes">
-				<h2>Ja</h2>
-				<p>Zorgt u voor iemand die extra hulp nodig heeft?</p>
+				<h1>Ja</h1>
 			</div>
 			<div class="col-md-12 mantelcheck-progress">
     			<h4>1/4</h4>
