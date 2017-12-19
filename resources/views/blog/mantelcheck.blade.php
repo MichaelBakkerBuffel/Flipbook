b88c3eeb1d42217a9a21804ff7f8100095f75587
LAPTOP-FABIAN\Fabian <fabsterdanoontjedevries@gmail.com>
2017-12-19 15:28:38 +0100

    Mantelcheck v2 (not even close to finished)

--- resources/views/blog/mantelcheck.blade.php
+++ resources/views/blog/mantelcheck.blade.php
@@ -2,23 +2,12 @@
 
 @section('content')
 
-
-	<div class="container">
-    	<div class="row">
-    		<div class="col-md-12 mantelcheck-title">
-    			<h4>Mantelzorgtest</h4>
-    		</div>
-    		<div class="col-md-12 mantelcheck-progress">
-    			<h4>1/4</h4>
-    		</div>
-    		<div class="col-md-6 mantelcheck-bodyblock mantelcheck-yes">
-				<h1>Ja</h1>
-				<p>Zorgt u voor iemand die extra hulp nodig heeft?</p>
-			</div>
-			<div class="col-md-6 mantelcheck-bodyblock mantelcheck-no">
-				<h1>Nee</h1>
-				<p>Zorgt u voor iemand die extra hulp nodig heeft?</p>
-			</div>
+	<div class="row">
+		<div class="mantelcheck-yes">
+			<img class="mantelcheck-yes-image" src="../img/mantelcheck_yes_image.jpg" alt="image">
+		</div>
+		<div class="mantelcheck-no">
+			<img class="mantelcheck-no-image" src="../img/mantelcheck_no_image.jpg" alt="image">
 		</div>
 	</div>
 
