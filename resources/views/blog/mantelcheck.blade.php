0aa428ef2795e48ccd8722a575c5bf14d811797b
LAPTOP-FABIAN\Fabian <fabsterdanoontjedevries@gmail.com>
2017-12-21 11:01:49 +0100

    Mantelcheck update

--- resources/views/blog/mantelcheck.blade.php
+++ resources/views/blog/mantelcheck.blade.php
@@ -2,12 +2,21 @@
 
 @section('content')
 
-	<div class="row">
+	<div class="row row-fix">
+		<div class="progressbar">
+			<p>yes</p>
+		</div>
 		<div class="mantelcheck-yes">
-			<img class="mantelcheck-yes-image" src="../img/mantelcheck_yes_image.jpg" alt="image">
+			<div class="mantelcheck-yes-text">
+				<h1>Ja</h1>
+				<p>Zorgt u voor iemand die extra hulp nodig heeft?</p>
+			</div>
 		</div>
 		<div class="mantelcheck-no">
-			<img class="mantelcheck-no-image" src="../img/mantelcheck_no_image.jpg" alt="image">
+			<div class="mantelcheck-no-text">
+				<h1>Nee</h1>
+				<p>Zorgt u voor iemand die extra hulp nodig heeft?</p>
+			</div>
 		</div>
 	</div>
 
